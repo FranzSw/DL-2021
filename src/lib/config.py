2153fd4a3c90a73bca14188b868b4b094324425f
Benedikt Weber <benedikt.web23@gmail.com>
2021-07-23 20:06:15 +0200

    Add advanced module demo

--- src/lib/config.py
+++ src/lib/config.py
@@ -5,15 +5,17 @@ from os import path
 class Config(object):
 
     def __init__(self, model):
-        self.mod = importlib.import_module(model)
-        self.mod.Evaluator.setup()
+        if type(model) == str:
+            model = importlib.import_module(model).Evaluator
+        self.evaluator = model
+        self.evaluator.setup()
         self.content_weight = 0.025
         self.style_weight = 12.0
         self.total_variation_weight = 10.0
         self.num_iterations = 10
 
     def set_content(self, img):
-        self.content = self.mod.Evaluator.preprocess_image(img)
+        self.content = self.evaluator.preprocess_image(img)
 
     def set_style(self, img):
-        self.style = self.mod.Evaluator.preprocess_image(img)
+        self.style = self.evaluator.preprocess_image(img)
